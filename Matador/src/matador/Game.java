fb693fad8c75d07c0a2bbeeb0333e6d842969798
Anders K <ande7353@gmail.com>
2014-02-06 09:51:10 +0100

    Initial commit

---
+++ Matador/src/matador/Game.java
@@ -0,0 +1,15 @@
+/*
+ * This weird stuff is made in educational purpose
+ * Use it to whatever you want BUT give us credit plz
+ * https://github.com/and3k5/Matador
+ * - Le Group Un
+ */
+
+package matador;
+
+/**
+ * @author Le Group Un
+ */
+public class Game {
+    
+}
