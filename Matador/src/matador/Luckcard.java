c23fd3fe7e59969cc4e474c72689129a8817f14c
TheDeadDragon <mjwt@live.dk>
2014-02-10 14:59:46 +0100

    more lucky cards

--- Matador/src/matador/Luckcard.java
+++ Matador/src/matador/Luckcard.java
@@ -17,7 +17,7 @@ public class Luckcard {
     
     static void TryCard(Player P)
     {
-        int[] Luckcards = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+        int[] Luckcards = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33};
         String Text;
         int length = Luckcards.length;
         int randomcard;
