00132e6627f0e9854778defbf813cdcc720a8f8d
TheDeadDragon <mjwt@live.dk>
2014-02-11 09:02:27 +0100

    few changes

--- Matador/src/matador/Prison.java
+++ Matador/src/matador/Prison.java
@@ -6,6 +6,8 @@
 
 package matador;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author and3k5
@@ -14,10 +16,22 @@ public class Prison extends Field{
     @Override
     public void Lands(Player P)
     {
-        
+        if(P.InPrison == true)
+        {
+            Prisoner(P);
+        }
     }
-    public void Prisoner()
+    private void Prisoner(Player P)
     {
+        Object[] options = new Object[3];
+        options[0] = "Kast med Terningerne";
+        options[1] = "Betal 1000";
+        int GetOutCard = 1;
+        if (GetOutCard > 0)
+        {
+            options[2] = "Brug lykke kort";
+        }
+        JOptionPane.showOptionDialog(null, "Tryk på en knap", "Valg",JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,null, options, options[0]);
         
     }
 }
